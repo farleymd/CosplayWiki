bdeba2811bbfa82828cbcf088bcdb8723d5538ea
farleymd <farleymd85@gmail.com>
2015-05-03 22:48:46 -0500

    Added Character class, return an arrayList when searching for a character

---
+++ Character.java
@@ -0,0 +1,27 @@
+package Marty.company;
+
+import java.util.StringTokenizer;
+
+/**
+ * Created by marty.farley on 5/3/2015.
+ */
+public class Character {
+    private int characterID;
+    private String characterName;
+    private String gender;
+    private int genreID;
+    private int universeID;
+    private int mediaID;
+    private String description;
+
+    public Character(int characterID, String characterName, String gender, int genreID, int universeID,
+                     int mediaID, String description){
+        this.characterID = characterID;
+        this.characterName = characterName;
+        this.gender = gender;
+        this.genreID = genreID;
+        this.universeID = universeID;
+        this.mediaID = mediaID;
+        this.description = description;
+    }
+}
